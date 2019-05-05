c03601755149adfb288ace2ab8f22cc3f5ca2bf4
MinjaeKim <f16junsu@snu.ac.kr>
2019-05-05 17:43:31 +0900

    a simple functioning bot

--- Testbot.py
+++ Testbot.py
@@ -3,16 +3,15 @@ import discord
 
 client = discord.Client()
 
-
-@client.event
+@client.event # event decorator/wrapper
 async def on_ready():
-    print(client.user.id)
-    print("ready")
-
+    print(f"We have logged in as {client.user}")
 
 @client.event
-async def on_message(messgage):
-    if message.content.startwith("안녕"):
-        await message.channel.send("안녕하세요!")
+async def on_message(message):
+    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+
+    if "hi there" in message.content.lower():
+        await message.channel.send("HI!")
 
 client.run("NTc0NDkyOTM0MTQ2MDk3MTY3.XM6NiA.tL1ouEK_j1SntPVzO9kH5agiPzI")
